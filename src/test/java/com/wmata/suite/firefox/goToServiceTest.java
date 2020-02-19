6dc334d207ffc8e99f71cfa30719a604b0548073
marwan <marwan.sultan@gmail.com>
2020-02-19 12:24:01 -0500

    WMATA demo.
    Signed-off-by: marwan <marwan.sultan@gmail.com>

---
+++ src/test/java/com/wmata/suite/firefox/goToServiceTest.java
@@ -0,0 +1,24 @@
+package com.wmata.suite.firefox;
+
+import com.pages.HomePage;
+import core.BaseTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class goToServiceTest extends BaseTest {
+
+	HomePage homepage;
+
+
+	@Test
+	public void goToService() {
+
+		HomePage homepage = new HomePage(driver);
+		homepage.clickServiceLink();
+		WebElement header = driver.findElement(By.cssSelector("#mm-0 > div.page-wrapper.landing.inner > div" +
+				".main-content > div.landing-header-wrapper > div > div.landing-header-content > h1"));
+		Assert.assertTrue(header.isDisplayed());
+	}
+}
